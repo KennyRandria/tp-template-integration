0f6f9773fa6ad4103e46adc066a1285eca7cc71a
KennyRandria <103984550+KennyRandria@users.noreply.github.com>
2022-07-06 23:27:09 +0300

    Update Axios.js

--- src/components/Axios/Axios.js
+++ src/components/Axios/Axios.js
@@ -7,3 +7,50 @@ export default function Modal(props){
     let url="https://jsonplaceholder.typicode.com/users";
     url+=(method==="put")?("/"+data.id):("");
     const dataValues = Object.values(data);
+    let onClick=()=>{
+        setShowModal(false);
+        window.removeEventListener("click",onClick);
+    }
+return (
+    <div className={"touchable"} onMouseLeave={()=>{
+        window.addEventListener("click",onClick);
+    }}
+    onMouseEnter={()=>{
+        window.removeEventListener("click",onClick);
+    }}
+    >
+        <form id={"formID"} >
+            <h3>Formulaire de type {method}</h3>
+            <p>*placeholder values are taken by input after no input and blur*</p>
+            {
+                dataValues.map((e)=>{
+                    if(typeof(e)==="object"){
+                        e=(e.name||e.city);
+                    }
+                    return (
+                        <input type={"text"} placeholder={e} onBlur={(event)=> {
+                            if(event.currentTarget.value.trim()===""){
+                                event.currentTarget.value=event.currentTarget.placeholder;
+                                formData[e]=event.currentTarget.value;
+                            }
+                        }}
+                        onChange={(event)=>{
+                            formData[e]=event.currentTarget.value;
+                            data[3]=formData[e];
+                        }}
+                        />
+                    )
+                })
+            }
+            <button type={"submit"} className={"btn btn-primary"} formAction={method} onClick={(e)=> {
+                e.preventDefault()
+                axios[method](url,formData).then((response)=>console.log(response)).catch((error)=>console.error(error));
+                setShowModal(false);
+            }}>{method==="post"?"add":"update"}</button>
+            <button type={"reset"} className={"btn btn-danger"}>reset</button>
+            <button type="reset" className={"btn btn-danger"} onClick={()=>setShowModal(false)}>Exit</button>
+        </form>
+
+    </div>
+)
+}
